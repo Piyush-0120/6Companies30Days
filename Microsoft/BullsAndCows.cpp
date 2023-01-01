9cd1d7f599ffbe8a9350319d1074bfd4fc1f92d8
piyush-0120 <piyushsharma1420@gmail.com>
2023-01-02 00:50:08 +0530

    modified:   Microsoft/BullsAndCows.cpp

--- Microsoft/BullsAndCows.cpp
+++ Microsoft/BullsAndCows.cpp
@@ -4,7 +4,7 @@ public:
     string getHint(string s, string g) {
         int a=0,b=0;
         // hashmap to store the frequency of digits
-        vector<int>h1(10,0),h2(11,0);
+        vector<int>h1(10,0),h2(10,0);
         for(int i=0;i<s.size();i++){
             if(s[i]==g[i])
                 a++;
