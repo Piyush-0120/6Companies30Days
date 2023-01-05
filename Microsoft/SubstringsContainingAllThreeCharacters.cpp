fb1fbc49378d729fe9e8535d24a875b786077f9b
piyush-0120 <piyushsharma1420@gmail.com>
2023-01-05 19:04:51 +0530

    new file:   Microsoft/SubstringsContainingAllThreeCharacters.cpp

---
+++ Microsoft/SubstringsContainingAllThreeCharacters.cpp
@@ -0,0 +1,35 @@
+/*
+
+Problem 15: Number of substrings containing all three characters
+https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/
+
+*/
+class Solution {
+public:
+    // sliding window
+    int numberOfSubstrings(string s) {
+        int n=s.size();
+        int ans=0;
+        int left=0,right=0;
+        int m[3];
+        m[0]=m[1]=m[2]=0;
+        
+        while(left<n){
+            
+            while(left<=right && right<n && !(m[0]>=1 && m[1]>=1 && m[2]>=1)){
+                m[s[right]-'a']++;
+                right++;
+                
+            }
+            if(m[0]>=1 && m[1]>=1 && m[2]>=1){
+                // cout << n-(right-1)<<"\n";
+                ans+=n-(right-1); // this range along with the elements in the right also are valid substrings
+            }
+            
+            m[s[left]-'a']--;
+            left++;
+        }
+        return ans;
+        
+    }
+};
